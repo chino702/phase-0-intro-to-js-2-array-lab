50d8438d1bcd28d0bb0f1664fa35db8daacf1afa
chino702 <chinox3702@gmail.com>
2023-03-05 14:56:44 -0800

    this is an array lab cats

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function(){cats.length = 0;
+
+cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyPrependCat(name){cats.unshift(name);}
+
+function destructivelyAppendCat(name){cats.push(name);};
+
+function destructivelyRemoveLastCat(name){cats.pop(name);};
+
+function destructivelyRemoveFirstCat(name){cats.shift(name);};
+
+function appendCat(name){return [...cats, name];};
+
+function prependCat(name){return[name, ...cats];};
+
+function removeLastCat(){return cats.slice(0, -1);};
+
+function removeFirstCat(){return cats.slice(1)};
